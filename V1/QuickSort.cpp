50dd01c03686a6ca7f3e344d0015eaf41928bac4
Amitesh Rana <95200309+Amitesh-Rana@users.noreply.github.com>
2023-08-05 05:40:23 +0530

    Create QuickSort.cpp

---
+++ V1/QuickSort.cpp
@@ -0,0 +1,93 @@
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include <algorithm>
+#include <random>
+#include <chrono>
+#include <thread>
+
+const int windowWidth = 800;
+const int windowHeight = 600;
+const int barWidth = 5;
+
+// Function to draw bars on the window with custom color
+void drawBars(sf::RenderWindow& window, std::vector<int>& bars, sf::Color barColor,
+    int comparingIdx = -1, int swappingIdx = -1) {
+    window.clear(sf::Color(240, 240, 240)); // Light gray background
+
+    for (size_t i = 0; i < bars.size(); i++) {
+        sf::RectangleShape bar(sf::Vector2f(barWidth, bars[i]));
+        bar.setPosition(i * barWidth, windowHeight - bars[i]);
+        if (i == comparingIdx || i == swappingIdx) {
+            bar.setFillColor(sf::Color::Magenta); // Highlight comparing or swapping bars
+        }
+        else {
+            bar.setFillColor(barColor);
+        }
+        window.draw(bar);
+    }
+
+    window.display();
+}
+
+// Partition function used in Quick Sort
+int partition(std::vector<int>& arr, int low, int high, sf::RenderWindow& window, sf::Color barColor, int sortingSpeed) {
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            std::swap(arr[i], arr[j]);
+            drawBars(window, arr, barColor, i, j);
+            std::this_thread::sleep_for(std::chrono::milliseconds(sortingSpeed));
+        }
+    }
+
+    std::swap(arr[i + 1], arr[high]);
+    drawBars(window, arr, barColor, i + 1, high);
+    std::this_thread::sleep_for(std::chrono::milliseconds(sortingSpeed));
+
+    return i + 1;
+}
+
+// Quick Sort algorithm (updated to enable smooth animation)
+void quickSort(std::vector<int>& arr, int low, int high, sf::RenderWindow& window, sf::Color barColor, int sortingSpeed) {
+    if (low < high) {
+        int pi = partition(arr, low, high, window, barColor, sortingSpeed);
+        quickSort(arr, low, pi - 1, window, barColor, sortingSpeed);
+        quickSort(arr, pi + 1, high, window, barColor, sortingSpeed);
+    }
+}
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "Sorting Visualizer");
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(50, windowHeight);
+
+    std::vector<int> bars(windowWidth / barWidth);
+    for (size_t i = 0; i < bars.size(); i++) {
+        bars[i] = dis(gen);
+    }
+
+    // Set initial color and sorting speed
+    sf::Color barColor = sf::Color::Cyan;
+    int sortingSpeed = 15;
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        // Update window
+        drawBars(window, bars, barColor);
+
+        // Sort the bars using Quick Sort
+        quickSort(bars, 0, bars.size() - 1, window, barColor, sortingSpeed);
+    }
+
+    return 0;
+}
